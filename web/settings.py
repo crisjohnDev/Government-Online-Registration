6076e4155a46ba76921a2ecc402c1b7896dc20b5
crisjohn.dev.official@gmail.com <crisjohn.dev.official@gmail.com>
2026-09-14 13:24:32 +0800

    adada

--- web/settings.py
+++ web/settings.py
@@ -146,3 +146,12 @@ MAILERS = {
 }
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
